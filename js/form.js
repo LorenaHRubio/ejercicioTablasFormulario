526f5b9a5da19724af734eb0e57daa910339c7d2
LorenaHRubio <eneachan@gmail.com>
2021-12-09 13:12:55 +0100

    m

--- js/form.js
+++ js/form.js
@@ -1,27 +1,25 @@
-import {clientInfo} from './clientInfo.js'
+import {clientInfo} from './clientInfo';
 
-export const form = () => {
-
-   const submitButton = document.getElementById('sbmt');
-
-   submitButton.addEventListener('click', (event) => {
-
-      event.preventDefault();
-
-      let reservationData = document.getElementById("reservation__data");
-      let formData = new FormData (reservationData); 
-
-      if( ckeditors != 'null'){
+export let buttonSend = () => {
+    const button = document.querySelector(".crud__store-button");
     
-         Object.entries(ckeditors).forEach(([key, value]) => {
-            formData.append(key, value.getData());
-         });
-      }
-      
-      formData.append('fingerprint', getFingerprint());
+    button.addEventListener('click', (event) => {
 
-      for (let pair of formData.entries()) {
-         console.log(pair[0]+ ', ' + pair[1]); 
-      }
-   });
-}
+        event.preventDefault();
+        let formElement = document.getElementById("crud__user-form");
+        formData = new FormData(formElement);
+        
+        if( ckeditors != 'null'){
+    
+            Object.entries(ckeditors).forEach(([key, value]) => {
+               formData.append(key, value.getData());
+            });
+         }
+         
+         formData.append('fingerprint', getFingerprint());
+        //ver las cosas en la consola que cogen del formulario con js
+        for (var pair of formData.entries()) {
+            console.log(pair[0]+ ', ' + pair[1]); 
+        }
+    });
+}  

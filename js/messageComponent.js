403f158374b0be9ba0ba1eca8cb47f7f0ce9bb91
LorenaHRubio <eneachan@gmail.com>
2021-12-16 19:34:20 +0100

    alertas

--- js/messageComponent.js
+++ js/messageComponent.js
@@ -35,6 +35,23 @@ class AlertMessage extends HTMLElement{
         if(name == 'type'){
             let alertMessage = this.shadow.querySelector("#alert-message");
             alertMessage.classList.add(newValue);
+            if(newValue == 'error'){
+                let alertsvg = this.shadow.querySelector("#svg-error");
+                alertsvg.classList.add("active");
+            }if(newValue == 'success'){
+                let alertsvg = this.shadow.querySelector("#svg-success");
+                alertsvg.classList.add("active");
+            }if(newValue == 'help'){
+                let alertsvg = this.shadow.querySelector("#svg-q");
+                alertsvg.classList.add("active");
+            }if(newValue == 'info'){
+                let alertsvg = this.shadow.querySelector("#svg-info");
+                alertsvg.classList.add("active");
+            }if(newValue == 'warning'){
+                let alertsvg = this.shadow.querySelector("#svg-warning");
+                alertsvg.classList.add("active");
+            }
+            
         }
     }
     
@@ -57,8 +74,16 @@ class AlertMessage extends HTMLElement{
                 z-index:1500;
                 box-shadow: 5px 5px 15px 5px #000000;
             }
-            #alert-message svg{
+            #alert-svg{
+                display: flex;
+                align-items: center;
                 margin-right: .5em;
+            }
+            #svg-error, #svg-success, #svg-q, #dvg-info, #dvg-warning {
+                display: none;
+            }
+            #svg-error.active, #svg-success.active, #svg-q.active, #dvg-info.active, #dvg-warning.active{
+                display: inherit;                
             }
             #alert-message.active{
                 opacity: 1;
@@ -83,30 +108,32 @@ class AlertMessage extends HTMLElement{
             }            
         </style>
         <div id="alert-message">
-            <div>
-                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M13,13H11V7H13M13,17H11V15H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
-                </svg>
-            </div>
-            <div>
-                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M0.41,13.41L6,19L7.41,17.58L1.83,12M22.24,5.58L11.66,16.17L7.5,12L6.07,13.41L11.66,19L23.66,7M18,7L16.59,5.58L10.24,11.93L11.66,13.34L18,7Z" />
-                </svg>        
-            </div>
-            <div>
-                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
-                </svg>
-            </div>
-            <div>    
-                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
-                </svg>
-            </div>
-            <div>
-                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
-                    <path fill="currentColor" d="M13 14H11V9H13M13 18H11V16H13M1 21H23L12 2L1 21Z" />
-                </svg>    
+            <div id="alert-svg">
+                <div id="svg-error">
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M13,13H11V7H13M13,17H11V15H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
+                    </svg>
+                </div>
+                <div id="svg-succes">
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M0.41,13.41L6,19L7.41,17.58L1.83,12M22.24,5.58L11.66,16.17L7.5,12L6.07,13.41L11.66,19L23.66,7M18,7L16.59,5.58L10.24,11.93L11.66,13.34L18,7Z" />
+                    </svg>        
+                </div>
+                <div id="svg-q">
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M15.07,11.25L14.17,12.17C13.45,12.89 13,13.5 13,15H11V14.5C11,13.39 11.45,12.39 12.17,11.67L13.41,10.41C13.78,10.05 14,9.55 14,9C14,7.89 13.1,7 12,7A2,2 0 0,0 10,9H8A4,4 0 0,1 12,5A4,4 0 0,1 16,9C16,9.88 15.64,10.67 15.07,11.25M13,19H11V17H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
+                    </svg>
+                </div>
+                <div id="dvg-info">    
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M13,9H11V7H13M13,17H11V11H13M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" />
+                    </svg>
+                </div>
+                <div id="dvg-warning">
+                    <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                        <path fill="currentColor" d="M13 14H11V9H13M13 18H11V16H13M1 21H23L12 2L1 21Z" />
+                    </svg>    
+                </div>
             </div>
             <p></p>
         </div>`;
